92545cc9134662148bebd555cfc0e4f8d4ff4e32
honmuteneha <honmuteneha@gmail.com>
2024-11-27 15:09:29 +0530

    react first project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+        <h1>Welcome to React Js first project</h1>
+          <button style={
+          {
+            backgroundColor:"blueviolet",
+            fontSize:"20px"
+          }
+        }
+        >Register</button> 
+       
+
+    </div>
+  );
+}
+
+export default App;
